b45f411ecdee52f04c2f92b43e9b5373feaf8776
Carlos Ballario <carlosballario@gmail.com>
2024-07-29 10:01:42 -0300

    Actualización del fin de semana

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
 import express, { json, urlencoded } from "express"
+import handlebars from "express-handlebars"
+import __dirname from "./utils/utils.js"
+
 const app = express()
 const PORT = 8080
+
 import productsRouter from "./routes/products.router.js"
 import cartsRouter from "./routes/carts.router.js"
 
-
-
 // Middlewares
-
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
+// Configurar handlebars
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+//Utilizar recursos estáticos
 
+app.use(express.static(__dirname, +  '/public'));
 
 app.use("/", cartsRouter)
 app.use("/", productsRouter)
 
-
 // LISTENER
-
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
 })
